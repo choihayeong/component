1321d3a6b3f89db5c97526b6e4a3b92b86379453
최하영 <hychoi318@gmail.com>
2022-05-27 18:29:44 +0900

    modify forms 수정, accordion, buttons 추가

--- scripts/common.js
+++ scripts/common.js
@@ -1,1 +1,29 @@
 // common.js
+/* ACCORDION */
+const accordion = document.querySelector('.accordion'),
+    accordionItem = accordion.querySelectorAll('li');
+
+function accordionItemClickEvent(ele) {
+    ele.target.classList.toggle('active');
+    ele.target.nextElementSibling.classList.toggle('active');
+}
+
+accordionItem.forEach(item => {
+    item.addEventListener('click', accordionItemClickEvent);
+});
+
+/* MODAL */
+const body = document.body,
+    btnModal = document.querySelector('#btnModal'),
+    btnModalClose = document.querySelector('.btn-modal');
+
+function modalBtnClickEvent(e) {
+    e.target.nextElementSibling.classList.toggle('active');
+    body.classList.add('dim');
+}
+function modalCloseEvent(e) {
+    console.log(e.target.dataset.modal);
+}
+
+btnModal.addEventListener('click', modalBtnClickEvent);
+btnModalClose.addEventListener('click', modalCloseEvent);
