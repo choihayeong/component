766fac1e6a59a36b6f8cb3c9ace307d9a13576de
Hayeong <hychoi318@gmail.com>
2024-02-14 17:58:52 +0900

    wip : dropdown 등 추가

--- assets/scripts/common.js
+++ assets/scripts/common.js
@@ -1,5 +1,19 @@
 // common.js
+/** body tag */
+const body = document.body;
+
 /* ACCORDION */
+function closeAllDisclosure() {
+  const openAccordionItem = document.querySelectorAll("details[open]");
+
+  for (let i = 0; i < openAccordionItem.length; i++) {
+    openAccordionItem[i].removeAttribute("open");
+  }
+}
+
+closeAllDisclosure();
+
+/* previdous accordion */
 const accordion = document.querySelector(".accordion"),
   accordionItem = accordion.querySelectorAll("li");
 
@@ -12,18 +26,52 @@ accordionItem.forEach((item) => {
   item.addEventListener("click", accordionItemClickEvent);
 });
 
+/* DROPDOWN */
+const dropdownBtnEl = document.querySelectorAll(".btn--dropdown");
+const dropdownEl = document.querySelectorAll(".dropdown");
+
+const toggleAnotherEvent = (el) => {
+  document.querySelectorAll(".btn--dropdown.active").forEach((item) => {
+    if (el !== item) {
+      item.click();
+    }
+  });
+};
+
+dropdownBtnEl.forEach((el, index) => {
+  el.addEventListener("click", (e) => {
+    toggleAnotherEvent(e);
+    el.classList.toggle("active");
+    el.nextElementSibling.classList.toggle("active");
+  });
+});
+
+window.addEventListener("click", (e) => {
+  let target = e.target;
+
+  if (!target.classList.contains("btn--dropdown")) {
+    dropdownBtnEl.forEach((el, index) => {
+      el.classList.remove("active");
+    });
+    dropdownEl.forEach((el, index) => {
+      el.classList.remove("active");
+    });
+  }
+});
+
 /* MODAL */
-const body = document.body,
-  btnModal = document.querySelector("#btnModal"),
-  btnModalClose = document.querySelector(".btn-modal");
+const btnModal = document.querySelector("#btnModal");
+const modalCloseEl = document.querySelector("#modalClose");
 
 function modalBtnClickEvent(e) {
   e.target.nextElementSibling.classList.toggle("active");
   body.classList.add("dim");
 }
 function modalCloseEvent(e) {
-  console.log(e.target.dataset.modal);
+  const modalEl = document.querySelector(`#${e.target.dataset.modal}`);
+  modalEl.classList.remove("active");
+  body.classList.remove("dim");
 }
 
 btnModal.addEventListener("click", modalBtnClickEvent);
-btnModalClose.addEventListener("click", modalCloseEvent);
+modalCloseEl.addEventListener("click", modalCloseEvent);
